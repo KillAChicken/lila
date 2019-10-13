88d51b70dfe210ee166ae6634646bbb9ac6b31ac
KillAChicken <KillAChicken@yandex.ru>
2019-10-13 17:38:13 +0200

    Add test cases for title property of the base component.

--- test/test_lila/core/test_base_component.py
+++ test/test_lila/core/test_base_component.py
@@ -48,3 +48,37 @@ def test_invalid_classes():
         Component(classes=1)
 
     assert error_info.value.args[0] == "Classes must be iterable with string values"
+
+
+@pytest.mark.parametrize(
+    argnames="title, expected_title",
+    argvalues=[
+        ("string title", "string title"),
+        (None, None),
+        (12, "12"),
+    ],
+    ids=[
+        "String",
+        "None",
+        "Integer",
+    ],
+)
+def test_title(title, expected_title):
+    """Check title of a component.
+
+    1. Create a component with different titles.
+    2. Get title of the component.
+    3. Check the title of the component.
+    """
+    component = Component(title=title)
+    assert component.title == expected_title, "Wrong title"
+
+
+def test_default_title():
+    """Check default title of a component.
+
+    1. Create a component without specifying a title.
+    2. Check the title of the component.
+    """
+    component = Component()
+    assert component.title is None, "Wrong title"
